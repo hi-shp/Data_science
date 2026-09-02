066d4a93babf3f72547134af20a8124e2af7528c
hi-shp <hishp@pusan.ac.kr>
2026-09-02 15:30:00 +0900

    Revert "refactor: 장애물 전달 파라미터 대신 is_direct 플래그로 목적지 직행 시 처음 기본 곡선, 웨이포인트 통과 시 곡률 곡선 깔끔하게 분기"
    This reverts commit b1d14fa14af2e8aeaa45d842b3707a938acb67d0.

--- test_success_rate.py
+++ test_success_rate.py
@@ -163,13 +163,13 @@ Elapsed Time:    {elapsed/60.0:.1f} minutes ({elapsed:.1f} seconds)
             if env.path_timer >= 0.01:
                 env.path_timer = 0
                 if env.current_wp is None:
-                    # 목적지까지 장애물이 없어 바로 목적지를 추종하는 경우: 곡률을 최소화한 처음 기본 곡선
+                    # 목적지까지 장애물이 없어 바로 직행하는 경우: 최소 곡률로 목적지 직진
                     goal = env.target
-                    env.bezier_path = make_bezier_path(env.boat_pos, env.boat_heading, goal, is_direct=True)
+                    env.bezier_path = make_bezier_path(env.boat_pos, env.boat_heading, goal, obstacles=None)
                 else:
-                    # 장애물 갭(웨이포인트) 통과 시: 부드럽게 돌아나가는 곡률 곡선
+                    # 웨이포인트(갭) 우회 통과 구간: 장애물을 넉넉히 둘러가도록 외측 굴곡 및 곡률 부여
                     goal = env.current_wp["pos"]
-                    env.bezier_path = make_bezier_path(env.boat_pos, env.boat_heading, goal, is_direct=False)
+                    env.bezier_path = make_bezier_path(env.boat_pos, env.boat_heading, goal, obstacles=env.dynamic_obstacles, boat_radius=env.boat_radius)
                     
                 if env.bezier_path is not None:
                     env.pursuit_target = pure_pursuit(env.bezier_path, env.boat_pos, lookahead=70)
@@ -177,7 +177,7 @@ Elapsed Time:    {elapsed/60.0:.1f} minutes ({elapsed:.1f} seconds)
                 if env.current_wp is not None and env.next_wp is not None:
                     vec = env.current_wp["pos"] - env.boat_pos
                     next_start_head = math.atan2(vec[1], vec[0])
-                    env.next_bezier_path = make_bezier_path(env.current_wp["pos"], next_start_head, env.next_wp["pos"], is_direct=False)
+                    env.next_bezier_path = make_bezier_path(env.current_wp["pos"], next_start_head, env.next_wp["pos"], obstacles=env.dynamic_obstacles, boat_radius=env.boat_radius)
                     if env.next_bezier_path is not None:
                         env.next_pursuit_target = pure_pursuit(env.next_bezier_path, env.current_wp["pos"], lookahead=75)
                 else:
